1c2aa64c56738081755b5603f60c3e70b073a3d0
tejaswishah1 <tejaswishah1998@gmail.com>
2020-11-06 16:01:15 +0530

    [Tejas] Add Create Address Book

---
+++ AddressBookData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingLINQ
+{
+
+    /// <summary>
+    /// Declaring the fields
+    /// </summary>
+    class AddressBookData
+    {
+        /// <summary>
+        /// First Name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// City
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// State
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Zip
+        /// </summary>
+        public string Zip { get; set; }
+
+        /// <summary>
+        /// Phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Email ID
+        /// </summary>
+        public string Email { get; set; }
+
+    }
+}
